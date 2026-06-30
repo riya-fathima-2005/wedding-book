b439f972e70cdb554617074309917a3434eca6e8
riya-fathima-2005 <riyaf8052@gmail.com>
2026-06-30 12:11:11 +0530

    fixed contact mail api

--- frontend/wedding/my-react-app/src/Components/Sign/Sign.jsx
+++ frontend/wedding/my-react-app/src/Components/Sign/Sign.jsx
@@ -53,7 +53,7 @@ const Sign = () => {
       setLoading(true);
 
       const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/signup/`,
+  "https://wedding-book.onrender.com/signup/",
         {
           method: "POST",
           headers: {
