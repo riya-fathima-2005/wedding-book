3eca486b1ca7f6aee5964c7841bd509fed5dddf7
riya-fathima-2005 <riyaf8052@gmail.com>
2026-07-06 12:01:07 +0530

    Fixed contact API URL

--- frontend/wedding/my-react-app/src/Components/Contacting/Ourcontact.jsx
+++ frontend/wedding/my-react-app/src/Components/Contacting/Ourcontact.jsx
@@ -25,18 +25,16 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-   const response = await fetch(
-  "http://127.0.0.1:8000/api/contact/",
-      {
-        method: "POST",
-
-        headers: {
-          "Content-Type": "application/json",
-        },
-
-        body: JSON.stringify(formData),
-      }
-    );
+  const response = await fetch(
+  "https://wedding-book.onrender.com/api/contact/",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  }
+);
 
     const data = await response.json();
 
